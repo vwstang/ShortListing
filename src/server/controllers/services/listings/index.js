d64b46bfa10d97e091e43bfc89c9abcc4cb196d9
vwstang <vwstang6@gmail.com>
2020-09-26 02:13:51 -0400

    Add select listing controller function

--- src/server/controllers/services/listings/index.js
+++ src/server/controllers/services/listings/index.js
@@ -1,5 +1,9 @@
 import db from "../../db/index";
 
+export const selListing = (payload) => {
+  // if (payload)
+}
+
 export const insListing = (payload) => {
   console.log(payload);
   db.get("listings").push({ url: payload.url }).write();
