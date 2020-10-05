c2e9ec80ac59acfdc629da6c3f1a57c2b3aae505
vwstang <vwstang6@gmail.com>
2020-10-05 17:51:19 -0400

    Add more properties to listing

--- src/server/controllers/services/listings/index.js
+++ src/server/controllers/services/listings/index.js
@@ -12,6 +12,20 @@ export const insListing = (payload) => {
     district: payload.district,
     ewmajor: payload.esmajor,
     nsmajor: payload.nsmajor,
+    mlsinfo: payload.mlsinfo,
+    virtualtour: payload.virtualtour,
+    sqftrange: payload.sqftrange,
+    frontage: payload.frontage,
+    lotdepth: payload.lotdepth,
+    bedrooms: payload.bedrooms,
+    dens: payload.dens,
+    bathrooms: payload.bathrooms,
+    basement: payload.basement,
+    parking: payload.parking,
+    exposure: payload.exposure,
+    listprice: payload.listprice,
+    comparable: payload.comparable,
+    proptax: payload.proptax,
     url: payload.url
   };
   db.get("listings").push(newListing).write();
