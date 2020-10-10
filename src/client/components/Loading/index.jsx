6127a6c47d8a8d7aab52c3ef6bdaa3e5736edfd2
vwstang <vwstang6@gmail.com>
2020-10-09 21:48:33 -0400

    Added loading page

---
+++ src/client/components/Loading/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+        margin: 0,
+        background: "dodgerblue",
+        color: "white"
+      }}
+    >
+      <h2>LOADING</h2>
+    </div>
+  );
+};
+
+export default Loading;
