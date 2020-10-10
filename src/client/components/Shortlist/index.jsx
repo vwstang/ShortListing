e367a78f86dd9588144f2e02e093e21e195e35ed
vwstang <vwstang6@gmail.com>
2020-10-09 21:49:16 -0400

    Updated show hide button styling

--- src/client/components/Shortlist/index.jsx
+++ src/client/components/Shortlist/index.jsx
@@ -3,6 +3,10 @@ import React from "react";
 const Shortlist = ({ shortlist, hideList, setHideList }) => {
   return (
     <aside className="shortlist-container">
+      <button
+        className={`showHideShortlist${hideList ? ' collapsed' : ''}`}
+        onClick={() => setHideList(!hideList)}
+      ></button>
       <h2 className="shortlist-title">Shortlisted</h2>
       {shortlist.length > 0 ? (
         shortlist.map((listing) => (
@@ -11,12 +15,6 @@ const Shortlist = ({ shortlist, hideList, setHideList }) => {
       ) : (
         <p>No listings shortlisted</p>
       )}
-      <button
-        className="showHideShortlist"
-        onClick={() => setHideList(!hideList)}
-      >
-        Hide
-      </button>
     </aside>
   );
 };
